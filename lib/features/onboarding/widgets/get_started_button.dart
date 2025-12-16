1f84d3f75537079c47a49cac453270764afd783b
chaimaa mezzaoui <chaimaamezzaoui21@gmail.com>
2025-12-17 00:57:59 +0300

    Initial Flutter project

---
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfirst/core/helpers/extensions.dart';
+import 'package:flutterfirst/features/login/ui/widgets/login_screen.dart';
+import 'package:flutterfirst/core/routing/routes.dart';
+import 'package:flutterfirst/core/theming/colors.dart';
+import 'package:flutterfirst/core/theming/styles.dart' show TextStyles;
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: (){
+      context.pushNamed(Routes.loginScreen);
+    }, 
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(ColorsManager.mainBlue), 
+      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      minimumSize: MaterialStateProperty.all(const Size(double.infinity,52)
+      ),
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+ ),
+      ),
+    ),
+    child: Text(
+      'Get Started',
+      style:TextStyles.font16WhiteSemiBold,
+      ),  
+    );
+  }
+}
